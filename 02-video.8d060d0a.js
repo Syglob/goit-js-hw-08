ed660bd46242d39b637f936dfa18077ef6ce7586
Syglob <85882951+Syglob@users.noreply.github.com>
2021-11-23 15:40:32 +0000

    Deploying to gh-pages from @ Syglob/goit-js-hw-08@4e5c90c8fd6f046aead431f5044fed05b4416bf8 🚀

--- 02-video.8d060d0a.js
+++ 02-video.8d060d0a.js
@@ -4,4 +4,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=t(require("lodash.throttle"));function t(e){return e&&e.__esModule?e:{default:e}}const n=document.querySelector("iframe"),o=new Vimeo.Player(n),r="videoplayer-current-time";function u(e){localStorage.setItem(r,e.seconds)}o.on("timeupdate",(0,e.default)(u,2e3)),o.setCurrentTime(localStorage.getItem(r)).then(function(e){}).catch(function(e){e.name});
 },{"lodash.throttle":"FNyO"}]},{},["iiAF"], null)
-//# sourceMappingURL=syglob.github.io/goit-js-hw-08/02-video.8d060d0a.js.map
+//# sourceMappingURL=/goit-js-hw-08/02-video.8d060d0a.js.map
